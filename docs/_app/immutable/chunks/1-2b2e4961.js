72367feb8a3874e563afefc41d6f84c0292f3f39
Tom Allnutt <tom@greeninfo.org>
2022-12-07 19:33:56 -0800

    build

--- docs/_app/immutable/chunks/1-2b2e4961.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-fd0dd3fe.js";export{t as component};
--- docs/_app/immutable/chunks/8-535a0b0a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/work/_page.svelte-cf18221b.js";export{t as component};
