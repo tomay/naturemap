5c6bb1db68a1129bc9a37ba263ce8ca040b1c6c6
Tom Allnutt <tom@greeninfo.org>
2022-12-30 10:26:32 -0800

    really build

---
+++ docs/_app/immutable/chunks/2-030420ac.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-28e45119.js";export{t as component};
---
+++ docs/_app/immutable/chunks/8-9398f7fe.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/work/_page.svelte-da1bba0b.js";export{t as component};
